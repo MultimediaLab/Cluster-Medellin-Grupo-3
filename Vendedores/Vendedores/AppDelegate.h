44f80385404eb83af0f144c087ad39ada2bf10ba
Giovanny Suarez <suarezkrac@gmail.com>
2014-09-02 18:01:48 -0500

    sql lite
    sql lite

---
+++ Vendedores/Vendedores/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Vendedores
+//
+//  Created by Profesor on 2/09/14.
+//  Copyright (c) 2014 Profesor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Vendedores/Vendedores/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Vendedores
+//
+//  Created by Profesor on 2/09/14.
+//  Copyright (c) 2014 Profesor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
